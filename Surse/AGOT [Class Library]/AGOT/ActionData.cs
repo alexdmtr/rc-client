0d63c6f19ce1348a42c5d889a8ab37273a6a898b
alex-dmtr <alex.dmtr@yahoo.com>
2015-08-03 11:32:01 +0300

    Added Class Libraries

---
+++ Surse/AGOT [Class Library]/AGOT/ActionData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable()]
+public class ActionData
+{
+    public BoardAction Action;
+    public BoardState FinalState;
+    public List<KeyValuePair<BoardAction, BoardState>> History;
+
+    public ActionData()
+    {
+        this.History = new List<KeyValuePair<BoardAction, BoardState>>();
+    }
+    public ActionData(BoardAction Action, BoardState FinalState, List<KeyValuePair<BoardAction, BoardState>> History)
+    {
+        this.Action = Action;
+        this.FinalState = FinalState;
+        this.History = History;
+    }
+};
---
+++ Surse/AGOT [Class Library]/AGOT/BoardAction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System;
+
+[Serializable()]
+public class BoardAction
+{
+    public string Type;
+    private DataTable ActionData;
+
+    public int? Player;
+    public int? Actor;
+    public int? Target;
+    public int? Parameter;
+
+    public BoardAction(string Type, int? Player, int? Actor, int? Target, int? Parameter)
+    {
+        this.Type = Type;
+        this.Player = Player;
+        this.Actor = Actor;
+        this.Target = Target;
+        this.Parameter = Parameter;
+
+        StringBuilder QueryText = new StringBuilder("SELECT * FROM Actions WHERE Type='");
+        QueryText.Append(Type);
+        QueryText.Append(@"'");
+        ActionData = SQL.Query(QueryText.ToString());
+    }
+
+    public static bool operator !=(BoardAction A, BoardAction B)
+    {
+        if (A.Player != B.Player)
+            return true;
+        if (A.Actor != B.Actor)
+            return true;
+        if (A.Target != B.Target)
+            return true;
+        if (A.Parameter != B.Parameter)
+            return true;
+        return false;
+    }
+    public static bool operator ==(BoardAction A, BoardAction B)
+    {
+        return !(A != B);
+    }
+}
